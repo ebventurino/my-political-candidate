6a10e2a7cede1ddceab33c7d1f5226f95e2f08fb
Emily Venturino <ebventurino@gmail.com>
2018-05-22 12:06:38 -0500

    added Javascript/html

--- myscripts.js
+++ myscripts.js
@@ -3,3 +3,10 @@ var newNode = document.createElement("section")
 var node = document.createTextNode("This is an article. This is a new article. This is a very new article")
 newNode.appendChild(node)
 document.getElementById("p--main").appendChild(newNode)
+
+
+var anchor = document.getElementById("congressional-district");
+console.log(anchor)  
+var att = document.createAttribute("article");        
+att.value = "5th Congressional District";     
+anchor.setAttributeNode(att)     
