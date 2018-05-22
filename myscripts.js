26ddc477ae8628665c7f482bc968f248a440fe36
Emily Venturino <ebventurino@gmail.com>
2018-05-22 11:41:19 -0500

    added Javascript

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,5 @@
-document.getElementById("section").innerHTML = "New text!";
+
+var newNode = document.createElement("section")
+var node = document.createTextNode("This is an article. This is a new article. This is a very new article")
+newNode.appendChild(node)
+document.getElementById("p--main").appendChild(newNode)
